01c2c275042b7cb335096a344c3d5d9d5b9ad28c
rochadoug <doug_cms@hotmail.com>
2024-07-22 00:06:28 -0300

    Removida pasta meuapp

--- meuapp/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-use Google\Cloud\Firestore\FirestoreClient;
-
-$projectid = 'bibliaquiz-konvict2024';
-
-$fireDb = new FirestoreClient(['projectId' => $projectid]);
-
-?>
